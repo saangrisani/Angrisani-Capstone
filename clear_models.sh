0da27c52b80cfd1c2793c3542bedff1628fdb712
saangrisani <143440246+saangrisani@users.noreply.github.com>
2025-09-02 08:15:44 +0000

    skeleton

---
+++ clear_models.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc" -delete
+find . -path "*.sqlite3" -delete
+python manage.py makemigrations
+python manage.py migrate
+#!/bin/sh
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc" -delete
+find . -path "*.sqlite3" -delete
+python manage.py makemigrations
+python manage.py migrate
